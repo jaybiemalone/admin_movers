fb398d9b95d712152336d040585136bd17047639
jaybiemalone <jaybiemalone3@gmail.com>
2024-10-18 21:33:24 -0800

    Message

--- facility.php
+++ facility.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movers.com</title>
+    <title>Movers</title>
     <link rel="icon" href="/Asset/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./style/index.css">
     <script type="text/javascript" src="app.js" defer></script>
