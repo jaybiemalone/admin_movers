8c3961d89b1597b58580d298f49fd1a9d82a4888
jaybiemalone <jaybiemalone3@gmail.com>
2024-10-20 12:51:08 -0800

    edit done

--- index.php
+++ index.php
@@ -13,14 +13,12 @@
         <img src="./Asset/loginbackground.png" alt="">
         <form id="login-form">
             <div class="logo-container">
-                <img src="./Asset/logo.png" alt="" width="180" height="60">
+                <img src="./Asset/logo.png" alt="" width="300" height="120">
             </div>
-            <h1>Login</h1>
-            <label for="email">Username</label>
+            <label for="email" require>Username</label>
             <input type="email" name="email" id="email" placeholder="Enter your email"><br>
-            <label for="password">Password</label>
+            <label for="password" require>Password</label>
             <input type="password" name="password" id="password" placeholder="Enter your password">
-            <p><a href="#">Forgot Password?</a></p>
             <button id="submit" type="button">Login</button>
         </form>
     </div>
